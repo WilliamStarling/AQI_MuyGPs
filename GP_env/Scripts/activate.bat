6c45305e4ac4ef5142cbe4905e8cc5ab959fb0a4
WilliamStarling <williamjonas@comcast.net>
2026-04-30 17:27:58 -0500

    reorganized and added readme
    reorganized the file structure, removed unnecessary files, created a readme with setup instructions and layout descriptions.

--- GP_env/Scripts/activate.bat
+++ GP_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=I:\School\CSC433_AdvancedAI\MuyGPs_Project\GP_env"
+set "VIRTUAL_ENV=I:\School\CSC433_AdvancedAI\AQI_MuyGPs\GP_env"
 
 if not defined PROMPT set PROMPT=$P$G
 
